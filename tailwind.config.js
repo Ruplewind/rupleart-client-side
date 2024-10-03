d8112681050348ef8c641a329f564c61c8168d81
Benson306 <bnkimtai@gmail.com>
2024-10-03 21:19:44 +0300

    Footer color

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,12 @@ module.exports = withMT({
     extend: {
       colors: {
         'primary': '#125570',
+        purple: {
+          950: '#4c1d95', // Add a custom purple-950 color (example hex)
+        },
+        violet: {
+          950: '#2e1065', // Add the custom violet-950 color (example hex)
+        }
       },
       fontFamily: {
         montserrat: ['Montserrat', 'sans-serif'],
