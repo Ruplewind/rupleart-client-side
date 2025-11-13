319fcec2001c6ed806a9636a6787da32aaf106f4
Benson306 <bnkimtai@gmail.com>
2025-11-13 13:09:08 +0300

    In transit

--- src/components/MyOrders.js
+++ src/components/MyOrders.js
@@ -97,7 +97,10 @@ function MyOrders() {
                             </td>
                             <td className='text-center p-2'>{
                                 ad.delivery_status == "pending" ? 
-                                    <div className='bg-gray-300 text-xs rounded-2xl p-1 w-full lg:w-3/4 mx-auto'>Pending</div>
+                                    <div className='bg-gray-300 text-xs rounded-2xl p-1 w-full lg:w-3/4 mx-auto'>Pending Confirmation</div>
+                                :
+                                ad.delivery_status == "transit" ? 
+                                    <div className='bg-orange-900 text-white text-xs rounded-2xl p-1 w-full lg:w-3/4 mx-auto'>In Transit</div>
                                 :
                                     <div className='bg-green-500 text-xs rounded-2xl p-1 w-full lg:w-3/4 mx-auto text-white'>Delivered</div>
                                 }
